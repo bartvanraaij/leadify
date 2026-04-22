2cfd823a0098c134e7919aef7defe59100c09ba2
Bart van Raaij <info@bartvanraaij.nl>
2026-04-22 11:35:43 +0200

    Remove ui-testing config

--- Leadify/LeadifyApp.swift
+++ Leadify/LeadifyApp.swift
@@ -9,30 +9,7 @@ struct LeadifyApp: App {
 
     init() {
         do {
-            #if DEBUG
-            if ProcessInfo.processInfo.arguments.contains("--uitesting") {
-                // Reset any persisted @AppStorage values so UI tests run deterministically.
-                // If --uitest-nav-mode=<rawValue> is passed, use that mode; else the default.
-                let navModeOverride = ProcessInfo.processInfo.arguments
-                    .first { $0.hasPrefix("--uitest-nav-mode=") }
-                    .map { $0.replacingOccurrences(of: "--uitest-nav-mode=", with: "") }
-                    .flatMap { PerformanceNavigationMode(rawValue: $0) }
-                UserDefaults.standard.set(
-                    (navModeOverride ?? .defaultMode).rawValue,
-                    forKey: PerformanceNavigationMode.storageKey
-                )
-
-                let config = ModelConfiguration(isStoredInMemoryOnly: true)
-                container = try ModelContainer(
-                    for: Song.self, Tacet.self, SetlistEntry.self, Setlist.self, Medley.self, MedleyEntry.self,
-                    configurations: config
-                )
-            } else {
-                container = try ModelContainer(for: Song.self, Tacet.self, SetlistEntry.self, Setlist.self, Medley.self, MedleyEntry.self)
-            }
-            #else
             container = try ModelContainer(for: Song.self, Tacet.self, SetlistEntry.self, Setlist.self, Medley.self, MedleyEntry.self)
-            #endif
         } catch {
             fatalError("Failed to create ModelContainer: \(error)")
         }
